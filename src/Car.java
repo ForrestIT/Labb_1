3de0282f784afd78d4091e5971341ba34bffb7b3
Johannes Gustavsson <Jogust95gmail.com>
2017-11-07 09:09:29 +0100

    Initial Commit

--- src/Car.java
+++ src/Car.java
@@ -6,11 +6,22 @@ public class Car {
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
+    public boolean turbo;
+    public boolean turboOn;
+    public static double trimFactor = 1;
 
     public Car (){
         stopEngine();
     }
+//--------------------- INC/DEC Speed ------------------------
+    public void incrementSpeed(double amount){
+    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+}
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - this.speedFactor() * amount,0);
+    }
 
+//---------------------- Start/Stop-engine ---------------------
     public void startEngine(){
         currentSpeed = 0.1;
     }
@@ -18,6 +29,23 @@ public class Car {
         currentSpeed = 0;
     }
 
+//---------------------- SpeedFactor ---------------------------------
+
+    public double speedFactor(){
+        double turbo = 1;
+        if(turboOn) turbo = 1.3;
+        return enginePower * 0.01 * turbo * trimFactor;
+    }
+
+//-------------------------- Turbo ------------------------------------
+    public void setTurboOn(){
+        turboOn = true;
+    }
+    public void setTurboOff(){
+        turboOn = false;
+    }
+
+//-------------------------- Peddals --------------------------------
     // TODO fix this method according to lab pm
     public void gas(double amount){
         incrementSpeed(amount);
@@ -39,6 +67,9 @@ public class Car {
     }
     public double getCurrentSpeed(){
         return currentSpeed;
+    }
+    public boolean getTurbo(){
+        return turbo;
     }
 
     //-------------Setters-----------------------------
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -2,26 +2,14 @@ import java.awt.*;
 
 public class Volvo240 extends Car{
 
-    public final static double trimFactor = 1.25;
-
-
     public Volvo240(){
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
         modelName = "Volvo240";
-    }
-    
-    public double speedFactor(){
-        return enginePower * 0.01 * trimFactor;
-    }
+        turbo = false;
+        trimFactor = 1.25;
 
-    public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
 
 }
